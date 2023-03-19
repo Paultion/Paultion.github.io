76a34f07885f28264e84f4302698b9f357f224b2
Paultion <15385862929@163.com>
2023-03-19 10:44:03 +0000

    Deploying to build from @ Paultion/Paultion.github.io@dcd8505e907203952ab2f27d77caa281f2f09153 🚀

---
+++ docs__js__css-1px.md.6c8e9050.async.js
@@ -0,0 +1,6 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[7060],{86066:function(a,t,_){_.r(t);var d=_(71100),s=_(35532),m=_(80543),h=_(73514),u=_(4046),r=_(67915),P=_(61414),i=_(66190),E=_(67294),e=_(85893);function o(){var l=(0,i.eL)(),n=l.texts;return(0,e.jsx)(i.dY,{children:(0,e.jsx)(e.Fragment,{children:(0,e.jsxs)("div",{className:"markdown",children:[(0,e.jsx)(u.Z,{lang:"html",children:n[0].value}),n[1].value,(0,e.jsx)("meta",{httpEquiv:"Content-Type",content:"text/html;charset=UTF-8"}),n[2].value,(0,e.jsx)("meta",{name:"viewport",content:"initial-scale=1, maximum-scale=1,minimum-scale=1, user-scalable=no "}),n[3].value,(0,e.jsx)("style",{dangerouslySetInnerHTML:{__html:`
+      * {
+        margin: 0;
+        padding: 0;
+      }
+    `}}),n[4].value,(0,e.jsx)("div",{style:{width:"50%",height:"10rem",fontSize:"2rem",border:"solid 1px red"},children:n[5].value}),n[6].value,(0,e.jsx)("div",{children:n[7].value}),n[8].value,(0,e.jsx)("script",{children:n[9].value}),n[10].value]})})})}t.default=o}}]);
